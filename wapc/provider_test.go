ad21b63d876b2a7364367e216ff56abc45a29dac
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-05-22 14:24:21 -0400

    Enable the acctest binary driver.

--- wapc/provider_test.go
+++ wapc/provider_test.go
@@ -3,11 +3,18 @@ package wapc
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 var testProviders = map[string]*schema.Provider{
 	"wapc": Provider(),
+}
+
+func TestMain(m *testing.M) {
+	acctest.UseBinaryDriver("wapc", Provider)
+	resource.TestMain(m)
 }
 
 func TestProvider(t *testing.T) {
