49b53a7b7960308d223103f3284e8bd69721e62b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-07-31 18:10:15 -0400

    Add dynamic data source configuration.

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320
+	github.com/hashicorp/go-plugin v1.3.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0
 	github.com/wapc/wapc-go v0.1.0
 	google.golang.org/grpc v1.30.0
--- provider/server.go
+++ provider/server.go
@@ -2,8 +2,10 @@ package provider
 
 import (
 	"context"
+	"log"
 
 	"github.com/EvilSuperstars/terraform-provider-wapc/tfplugin5"
+	"github.com/hashicorp/go-cty/cty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -19,61 +21,143 @@ func NewGRPCProviderServer() tfplugin5.ProviderServer {
 	}
 }
 
+//
+// ProviderServer interface methods.
+//
+
 //////// Information about what a provider supports/expects
 
 func (s *grpcProviderServer) GetSchema(ctx context.Context, req *tfplugin5.GetProviderSchema_Request) (*tfplugin5.GetProviderSchema_Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::GetSchema not implemented")
+	log.Println("[DEBUG] Enter ProviderServer::GetSchema")
+
+	oType, err := cty.DynamicPseudoType.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+
+	sType, err := cty.String.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &tfplugin5.GetProviderSchema_Response{
+		DataSourceSchemas: map[string]*tfplugin5.Schema{
+			"wapc_module": {
+				Version: 1,
+				Block: &tfplugin5.Schema_Block{
+					Attributes: []*tfplugin5.Schema_Attribute{
+						{
+							Name:     "filename",
+							Type:     sType,
+							Required: true,
+						},
+						{
+							Name:     "operation",
+							Type:     sType,
+							Required: true,
+						},
+						{
+							Name:     "input",
+							Type:     oType,
+							Required: true,
+						},
+						{
+							Name:     "result",
+							Type:     oType,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	log.Println("[DEBUG] Exit ProviderServer::GetSchema")
+	return resp, nil
 }
 
 func (s *grpcProviderServer) PrepareProviderConfig(ctx context.Context, req *tfplugin5.PrepareProviderConfig_Request) (*tfplugin5.PrepareProviderConfig_Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::PrepareProviderConfig not implemented")
+	log.Println("[DEBUG] Enter ProviderServer::PrepareProviderConfig")
+
+	log.Println("[DEBUG] Exit ProviderServer::PrepareProviderConfig")
+	return &tfplugin5.PrepareProviderConfig_Response{}, nil
 }
 
 func (s *grpcProviderServer) ValidateResourceTypeConfig(ctx context.Context, req *tfplugin5.ValidateResourceTypeConfig_Request) (*tfplugin5.ValidateResourceTypeConfig_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::ValidateResourceTypeConfig")
+
+	log.Println("[DEBUG] Exit ProviderServer::ValidateResourceTypeConfig")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::ValidateResourceTypeConfig not implemented")
 }
 
 func (s *grpcProviderServer) ValidateDataSourceConfig(ctx context.Context, req *tfplugin5.ValidateDataSourceConfig_Request) (*tfplugin5.ValidateDataSourceConfig_Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::ValidateDataSourceConfig not implemented")
+	log.Println("[DEBUG] Enter ProviderServer::ValidateDataSourceConfig")
+
+	log.Println("[DEBUG] Exit ProviderServer::ValidateDataSourceConfig")
+	return &tfplugin5.ValidateDataSourceConfig_Response{}, nil
 }
 
 func (s *grpcProviderServer) UpgradeResourceState(ctx context.Context, req *tfplugin5.UpgradeResourceState_Request) (*tfplugin5.UpgradeResourceState_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::UpgradeResourceState")
+
+	log.Println("[DEBUG] Exit ProviderServer::UpgradeResourceState")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::UpgradeResourceState not implemented")
 }
 
 //////// One-time initialization, called before other functions below
 
 func (s *grpcProviderServer) Configure(ctx context.Context, req *tfplugin5.Configure_Request) (*tfplugin5.Configure_Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::Configure not implemented")
+	log.Println("[DEBUG] Enter ProviderServer::Configure")
+
+	log.Println("[DEBUG] Exit ProviderServer::Configure")
+	return &tfplugin5.Configure_Response{}, nil
 }
 
 //////// Managed Resource Lifecycle
 
 func (s *grpcProviderServer) ReadResource(ctx context.Context, req *tfplugin5.ReadResource_Request) (*tfplugin5.ReadResource_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::ReadResource")
+
+	log.Println("[DEBUG] Exit ProviderServer::ReadResource")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::ReadResource not implemented")
 }
 
 func (s *grpcProviderServer) PlanResourceChange(ctx context.Context, req *tfplugin5.PlanResourceChange_Request) (*tfplugin5.PlanResourceChange_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::PlanResourceChange")
+
+	log.Println("[DEBUG] Exit ProviderServer::PlanResourceChange")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::PlanResourceChange not implemented")
 }
 
 func (s *grpcProviderServer) ApplyResourceChange(ctx context.Context, req *tfplugin5.ApplyResourceChange_Request) (*tfplugin5.ApplyResourceChange_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::ApplyResourceChange")
+
+	log.Println("[DEBUG] Exit ProviderServer::ApplyResourceChange")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::ApplyResourceChange not implemented")
 }
 
 func (s *grpcProviderServer) ImportResourceState(ctx context.Context, req *tfplugin5.ImportResourceState_Request) (*tfplugin5.ImportResourceState_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::ImportResourceState")
+
+	log.Println("[DEBUG] Exit ProviderServer::ImportResourceState")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::ImportResourceState not implemented")
 }
 
 func (s *grpcProviderServer) ReadDataSource(ctx context.Context, req *tfplugin5.ReadDataSource_Request) (*tfplugin5.ReadDataSource_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::ReadDataSource")
+
+	log.Println("[DEBUG] Exit ProviderServer::ReadDataSource")
 	return nil, status.Errorf(codes.Unimplemented, "ProviderServer::ReadDataSource not implemented")
 }
 
 //////// Graceful Shutdown
 
 func (s *grpcProviderServer) Stop(_ context.Context, _ *tfplugin5.Stop_Request) (*tfplugin5.Stop_Response, error) {
+	log.Println("[DEBUG] Enter ProviderServer::Stop")
+
 	close(s.stopCh)
 	s.stopCh = make(chan struct{})
 
+	log.Println("[DEBUG] Exit ProviderServer::Stop")
 	return &tfplugin5.Stop_Response{}, nil
 }
