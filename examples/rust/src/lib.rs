8ebae4eeb75d2de996e5a1cba27841a8025401f2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-08-04 16:03:38 -0400

    Input and result are JSON.

--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate wapc_guest as guest;
 
 use guest::prelude::*;
+use serde_json::json;
 
 wapc_handler!(handle_wapc);
 
@@ -14,6 +15,18 @@ fn handle_wapc(operation: &str, msg: &[u8]) -> CallResult {
 fn hello(msg: &[u8]) -> CallResult {
     guest::console_log("[DEBUG] Enter hello");
 
+    let t = match serde_json::from_slice(msg)? {
+        serde_json::Value::Array(_) => "Array",
+        serde_json::Value::Bool(_) => "Bool",
+        serde_json::Value::Null => "Null",
+        serde_json::Value::Number(_) => "Number",
+        serde_json::Value::Object(_) => "Object",
+        serde_json::Value::String(_) => "String",
+    };
+    guest::console_log(&format!("[DEBUG] JSON {}", t));
+
+    let result = json!({"key": 123});
+
     guest::console_log("[DEBUG] Exit hello");
-    Ok(vec![])
+    Ok(serde_json::to_vec(&result)?)
 }
