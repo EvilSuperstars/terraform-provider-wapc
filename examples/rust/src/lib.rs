a139837938343a1b7493496d834ca269c123f43c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-08-04 15:35:34 -0400

    Most basic waPC guest in Rust.

--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate wapc_guest as guest;
+
+use guest::prelude::*;
+
+wapc_handler!(handle_wapc);
+
+fn handle_wapc(operation: &str, msg: &[u8]) -> CallResult {
+    match operation {
+        "hello" => hello(msg),
+        op => Err(format!("unknown operation {}", op).into()),
     }
+}
+
+fn hello(msg: &[u8]) -> CallResult {
+    guest::console_log("[DEBUG] Enter hello");
+
+    guest::console_log("[DEBUG] Exit hello");
+    Ok(vec![])
 }
