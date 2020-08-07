d50df543f98d043bcfda333673ed880a9de24312
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-08-07 14:21:28 -0400

    More sophisticated Rust example.

--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate wapc_guest as guest;
 
 use guest::prelude::*;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 wapc_handler!(handle_wapc);
@@ -12,21 +13,39 @@ fn handle_wapc(operation: &str, msg: &[u8]) -> CallResult {
     }
 }
 
+//
+// Can be specified in HCL as:
+// input = {
+//   "Name"      = "waPC"
+//   "Uppercase" = true
+// }
+//
+#[derive(Serialize, Deserialize)]
+struct Input {
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Uppercase")]
+    #[serde(default)]
+    uppercase: bool,
+}
+
 fn hello(msg: &[u8]) -> CallResult {
     guest::console_log("[DEBUG] Enter hello");
 
-    let t = match serde_json::from_slice(msg)? {
-        serde_json::Value::Array(_) => "Array",
-        serde_json::Value::Bool(_) => "Bool",
-        serde_json::Value::Null => "Null",
-        serde_json::Value::Number(_) => "Number",
-        serde_json::Value::Object(_) => "Object",
-        serde_json::Value::String(_) => "String",
+    let result = match serde_json::from_slice(msg) {
+        Ok(input @ Input { .. }) => {
+            let mut name = input.name;
+            if input.uppercase {
+                name = name.to_uppercase();
+            }
+            //
+            // Can be referenced in HCL as `result["Name"]`.
+            //
+            serde_json::to_vec(&json!({ "Name": name }))?
+        }
+        _ => vec![],
     };
-    guest::console_log(&format!("[DEBUG] JSON {}", t));
-
-    let result = json!({"key": 123});
 
     guest::console_log("[DEBUG] Exit hello");
-    Ok(serde_json::to_vec(&result)?)
+    Ok(result)
 }
