cdb826c937bef4af25189fd18c1000544f30194b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-01-02 16:32:08 -0500

    No need for 'hashicorp/terraform-plugin-mux'.

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"context"
 	"log"
 	"os"
 
 	"github.com/EvilSuperstars/terraform-provider-wapc/provider"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5/server"
-	tfmux "github.com/hashicorp/terraform-plugin-mux"
 )
 
 func main() {
-	ctx := context.Background()
-
-	factory, err := tfmux.NewSchemaServerFactory(ctx, provider.Server)
-	if err != nil {
-		log.Println(err.Error())
-		os.Exit(1)
-	}
-
-	err = tf5server.Serve("wapc", func() tfprotov5.ProviderServer {
-		return factory.Server()
-	})
-	if err != nil {
+	if err := tf5server.Serve("wapc", provider.Server); err != nil {
 		log.Println(err.Error())
 		os.Exit(1)
 	}
